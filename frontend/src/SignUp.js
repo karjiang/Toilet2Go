50657cecce443dbd90232f04b7db45beb9f4cda0
Karl Jiang <karljian@usc.edu>
2024-06-05 00:03:34 -0700

    fixed small error

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { registerUser } from './api';
 
 const SignUp = ({ navigation }) => {
