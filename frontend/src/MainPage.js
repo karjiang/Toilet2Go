77c408fe005ae478a8ea40216deaca714b24733a
Karl Jiang <karljian@usc.edu>
2024-06-05 20:04:38 -0700

    Added zoom buttons, removed animation from beginning

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import MapboxGL from '@rnmapbox/maps';
 import { MAPBOX_ACCESS_TOKEN } from '@env';
 
-// Set the Mapbox access token]
+// Set the Mapbox access token
 MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
-console.log('Mapbox Access Token:', MAPBOX_ACCESS_TOKEN); // Add this line to debug
-
 const MainPage = () => {
+  const cameraRef = useRef(null);
+  const [zoomLevel, setZoomLevel] = useState(14);
+
+  const handleZoomIn = () => {
+    const newZoomLevel = Math.min(zoomLevel + 1, 20);
+    setZoomLevel(newZoomLevel);
+    cameraRef.current.setCamera({
+      zoomLevel: newZoomLevel,
+      animationDuration: 300,
+    });
+  };
+
+  const handleZoomOut = () => {
+    const newZoomLevel = Math.max(zoomLevel - 1, 0);
+    setZoomLevel(newZoomLevel);
+    cameraRef.current.setCamera({
+      zoomLevel: newZoomLevel,
+      animationDuration: 300,
+    });
+  };
+
   return (
     <View style={styles.container}>
       <MapboxGL.MapView style={styles.map}>
         <MapboxGL.Camera
-          zoomLevel={14}
-          centerCoordinate={[-118.2842911, 34.0213524]} // Coordinates for New York City
+          defaultSettings={{
+            centerCoordinate: [-118.2842911, 34.0213524], // Coordinates for Los Angeles
+            zoomLevel: 14,
+          }}
+          ref={cameraRef}
+          zoomLevel={zoomLevel}
         />
       </MapboxGL.MapView>
+      <View style={styles.zoomControls}>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomIn}>
+          <Text style={styles.zoomText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomOut}>
+          <Text style={styles.zoomText}>-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -27,6 +58,25 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  zoomControls: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    flexDirection: 'column',
+  },
+  zoomButton: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 2,
+  },
+  zoomText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
